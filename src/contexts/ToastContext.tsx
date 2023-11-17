14b321ceaace0ec5b655e6162f8a62b6780a3906
antonio <antonio@ovex.io>
2023-11-17 10:55:38 +0200

    comments not needed

--- src/contexts/ToastContext.tsx
+++ src/contexts/ToastContext.tsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
-// import 'react-toastify/dist/ReactToastify.css'
 
 interface IToastProviderProps {
   children: JSX.Element
